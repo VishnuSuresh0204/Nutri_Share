47a12504f2d12884e6fc6db91f32b21ea4054f7a
Vishnu Suresh <vish3eyedman.all@gmail.com>
2026-06-11 21:55:01 +0530

    chore: register models in Django admin

---
+++ nutri/myapp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Login)
+admin.site.register(UserProfile)
+admin.site.register(DonorProfile)
+admin.site.register(VolunteerProfile)
+admin.site.register(FoodDonation)
+admin.site.register(FoodRequest)
+admin.site.register(VolunteerAssignment)
+admin.site.register(DistributionRecord)
+admin.site.register(Feedback)
+admin.site.register(Complaint)
+admin.site.register(Notification)
