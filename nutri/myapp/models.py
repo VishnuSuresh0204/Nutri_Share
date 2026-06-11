5d241ddfe91e1f0aa9362d4fbbb815e16c47f032
Vishnu Suresh <vish3eyedman.all@gmail.com>
2026-06-11 21:55:01 +0530

    feat: add Notification model

--- nutri/myapp/models.py
+++ nutri/myapp/models.py
@@ -228,3 +228,21 @@ class Complaint(models.Model):
 
     def __str__(self):
         return self.subject
+
+# ---------------- NOTIFICATIONS ---------------- #
+
+class Notification(models.Model):
+    title = models.CharField(max_length=200)
+
+    message = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    target_role = models.CharField(
+        max_length=50
+    )  # donor / volunteer / user / all
+
+    def __str__(self):
+        return self.title
