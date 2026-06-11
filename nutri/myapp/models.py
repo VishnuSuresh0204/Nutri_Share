422d6988d4be6614cf852a0e0b24c45d48bab91d
Vishnu Suresh <vish3eyedman.all@gmail.com>
2026-06-11 21:54:18 +0530

    feat: add Login model

---
+++ nutri/myapp/models.py
@@ -0,0 +1,11 @@
+﻿from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# ---------------- LOGIN ---------------- #
+
+class Login(AbstractUser):
+    userType = models.CharField(max_length=50)  # admin / donor / volunteer / user
+    viewPass = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
